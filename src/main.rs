5ed684517d39ca51c0e713e7d07a00714d5bfa24
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-02-09 16:22:27 +0100

    response mapper (middlewere)

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,28 @@
 
 use axum::{
     extract::{Path, Query},
-    response::{Html, IntoResponse},
-    routing::get,
+    middleware,
+    response::{Html, IntoResponse, Response},
+    routing::{get, get_service},
     Router,
 };
 use serde::Deserialize;
+use tower_cookies::CookieManagerLayer;
+use tower_http::services::ServeDir;
+
+pub use self::error::{Error, Result};
+
+mod error;
+mod web;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().merge(routes_hello());
+    let routes_all = Router::new()
+        .merge(routes_hello())
+        .merge(web::routes_login::routes())
+        .layer(middleware::map_response(main_response_mapper))
+        .layer(CookieManagerLayer::new())
+        .fallback_service(routes_static());
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
         .await
@@ -18,15 +31,26 @@ async fn main() {
 
     println!("listening on {:?}", listener.local_addr());
 
-    axum::serve(listener, app.into_make_service())
+    axum::serve(listener, routes_all.into_make_service())
         .await
         .unwrap();
+}
+
+async fn main_response_mapper(res: Response) -> Response {
+    println!("->> {:<12} - main_response_mapper", "RES_MAPPER");
+    println!();
+    res
 }
 
 fn routes_hello() -> Router {
     Router::new()
         .route("/hello", get(handler_hello))
         .route("/hello2/:name", get(handler_hello2))
+}
+
+fn routes_static() -> Router {
+    println!("->> {:<12} - routes_static", "CALLED");
+    Router::new().nest_service("/", get_service(ServeDir::new("./")))
 }
 
 #[derive(Debug, Deserialize)]
