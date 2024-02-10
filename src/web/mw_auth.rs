fdd52c51d23dc3f8b6b294fa9f74160edb4c4640
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-02-10 14:45:52 +0100

    auth init

---
+++ src/web/mw_auth.rs
@@ -0,0 +1,36 @@
+use axum::body::Body;
+use axum::http::Request;
+use axum::middleware::Next;
+use axum::response::Response;
+use lazy_regex::regex_captures;
+use tower_cookies::Cookies;
+
+use crate::web::AUTH_TOKEN;
+use crate::{Error, Result};
+
+pub async fn mw_require_auth(cookies: Cookies, req: Request<Body>, next: Next) -> Result<Response> {
+    println!("->> {:<12} - my_require_auth", "MIDDLEWARE");
+
+    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+
+    // TODO: Real auth-token parsing & validation.
+    auth_token.ok_or(Error::AuthFailNoAuthTokenCookie)?;
+
+    Ok(next.run(req).await)
+}
+
+/// Parse a token of format `user-[user-id].[expiration].[signature]`
+/// Returns (user_id, expiration, signature)
+fn parse_token(token: String) -> Result<(u64, String, String)> {
+    let (_whole, user_id, exp, sign) = regex_captures!(
+        r#"^user-(\d+)\.(.+)\.(.+)"#, // a literal regex
+        &token
+    )
+    .ok_or(Error::AuthFailTokenWrongFormat)?;
+
+    let user_id: u64 = user_id
+        .parse()
+        .map_err(|_| Error::AuthFailTokenWrongFormat)?;
+
+    Ok((user_id, exp.to_string(), sign.to_string()))
+}
